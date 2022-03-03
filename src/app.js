1481df53995574cac5519ce6d5616d9466c180ff
renaatosalgado <renaato.salgado@gmail.com>
2022-03-03 12:43:38 -0300

    server structure deploy fix

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use(json());
 app.use(router);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on " + process.env.PORT);
+  console.log("Server running on port " + process.env.PORT);
 });

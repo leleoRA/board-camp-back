c7edc0ef41aa22826d618a89a038c917c0373e2b
renaatosalgado <renaato.salgado@gmail.com>
2022-03-04 11:17:05 -0300

    feat: POST /customers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -4,6 +4,26 @@ export async function getCustomers(req, res) {}
 
 export async function getCustomer(req, res) {}
 
-export async function createCustomer(req, res) {}
+export async function createCustomer(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
+  const hasCpf = await db.query(
+    `
+    SELECT * FROM customers WHERE cpf = $1
+  `,
+    [cpf]
+  );
+
+  if (hasCpf.rowCount > 0) return res.sendStatus(409);
+
+  await db.query(
+    `
+    INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)
+  `,
+    [name, phone, cpf, birthday]
+  );
+
+  res.sendStatus(201);
+}
 
 export async function updateCustomer(req, res) {}
--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -4,13 +4,13 @@ const customerSchema = joi.object({
   name: joi.string().required(),
   phone: joi
     .string()
-    .pattern(/^[0-9]{11}$/)
+    .pattern(/^[0-9]{10,11}$/)
     .required(),
   cpf: joi
     .string()
     .pattern(/^[0-9]{11}$/)
     .required(),
-  birthday: joi.string().pattern(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/),
+  birthday: joi.string().pattern(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/),
 });
 
 export default customerSchema;
